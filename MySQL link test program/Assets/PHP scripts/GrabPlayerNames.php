54d2126f6f2a185e70c6d9f6329beb9981f5e1f7
Kyle Brewer <kyle.brewer23@my.northampton.ac.uk>
2026-05-22 10:05:52 +0100

    project did not include all needed PHP scripts

--- MySQL link test program/Assets/PHP scripts/GrabPlayerNames.php
+++ MySQL link test program/Assets/PHP scripts/GrabPlayerNames.php
@@ -7,13 +7,12 @@ $dbname = "userinformation";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT username, Level, kills, Deaths, Rank FROM users" ;
+$sql = "SELECT username FROM users";
 
 // Execute the SQL query
 $result = $conn->query($sql);
@@ -25,13 +24,8 @@ if ($result->num_rows > 0)
   while($row = $result->fetch_assoc()) 
   {
     echo $row["username"]. "/";
-    echo $row["Level"]. "/";
-    echo $row["kills"]. "/";
-    echo $row["Deaths"]. "/";
-    echo $row["Rank"]. "/";
   }
 } 
-else { echo "0 results"; }
 
 $conn->close();
 ?>
